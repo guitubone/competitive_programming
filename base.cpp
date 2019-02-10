98b10b467f188297c1de363de9519413bd956ac4
Guilherme Tubone <ghtubone@gmail.com>
2019-02-10 15:25:18 -0200

    Added RNG

--- base.cpp
+++ base.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define se second
 typedef long long ll;
 typedef pair<int, int> ii;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int main(void) {
 	ios_base::sync_with_stdio(false); cin.tie(0);
--- segtree.cpp
+++ segtree.cpp
@@ -43,33 +43,33 @@ int n;
 int t[2 * N];
 
 void build() {
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+	for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
 }
 
 //Range Query and Point Update
 void modify(int p, int value) {
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+	for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
 }
 
 int query(int l, int r) {  // sum on interval [l, r)
-  int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
-  }
-  return res;
+	int res = 0;
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		if (l&1) res += t[l++];
+		if (r&1) res += t[--r];
+	}
+	return res;
 }
 
 //Range Update and Point Query
 void modify(int l, int r, int value) {
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) t[l++] += value;
-    if (r&1) t[--r] += value;
-  }
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		if (l&1) t[l++] += value;
+		if (r&1) t[--r] += value;
+	}
 }
 
 int query(int p) {
-  int res = 0;
-  for (p += n; p > 0; p >>= 1) res += t[p];
-  return res;
+	int res = 0;
+	for (p += n; p > 0; p >>= 1) res += t[p];
+	return res;
 }
